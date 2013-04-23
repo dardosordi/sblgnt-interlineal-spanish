b4c874365dbff043ab9d238cb4d1c3432869c216
Dardo Sordi <dardosordi@gmail.com>
2013-04-23 01:58:57 -0300

    generacion de links dinamica en strongs_number

--- php/strongs_number.php
+++ php/strongs_number.php
@@ -56,14 +56,19 @@ if (isset($strongs[$lang][$number])) {
 	$lexicon['biblestudytools'] = sprintf('http://www.biblestudytools.net/Lexicons/Greek/grk.cgi?search=%s&version=nas', $number);
 	$lexicon['logeion'] = sprintf('http://logeion.uchicago.edu/index.html#%s', urlencode($data['lemma']));
 	$lexicon['lmpg'] = sprintf('http://dge.cchs.csic.es/lmpg/%s', urlencode($data['lemma']));
+	$lexicon['greeklexicon'] = sprintf('http://greeklexicon.org/lexicon/strongs/%s/', urlencode($number));
+
 
 	$lex_url = 'http://biblestudyaids.net/nt/a&g/';
 	$lex_url = '/lexicon/';
 	$lexicon['DAG'] = $lex_url . $lexicon_index[$number];
 
+	$links = array();
+	foreach($lexicon as $key => $url) {
+		$links[] = sprintf('<a href="%s" target="_blank">%s</a>', h($url), h($key));
+	}
 
-	$content .= sprintf('	<dt>Lexicos:</dt><dd><a href="%s" target="_blank">perseus</a>, <a href="%s" target="_blank">studybible.info</a>, <a href="%s" target="_blank">blueletterbible.org</a>, <a href="%s" target="_blank">katabiblon.com</a>, <a href="%s" target="_blank">biblos.com</a>, <a href="%s" target="_blank">greek-dictionary.net</a>, <a href="%s" target="_blank">studylight.org</a>, <a href="%s" target="_blank">biblestudytools.net</a>, <a href="%s" target="_blank">logeion</a>, <a href="%s" target="_blank">lmpg</a>, <a href="%s" target="_blank">DAG</a></dd>', 
-		h($lexicon['perseus']), h($lexicon['studybible']), h($lexicon['blueletterbible']), h($lexicon['katabiblon']), h($lexicon['biblos']), h($lexicon['greek-dictionary']), h($lexicon['studylight']), h($lexicon['biblestudytools']), h($lexicon['logeion']), h($lexicon['lmpg']), h($lexicon['DAG']));
+	$content .= sprintf('	<dt>Lexicos:</dt><dd>%s</dd>', implode(', ', $links));
 
 	$content .= sprintf('	<dt>Audio:</dt><dd><audio src="/ogg/grk/%04dg.ogg" class="player" onclick="this.play();"></audio></dd>', $number);
 
