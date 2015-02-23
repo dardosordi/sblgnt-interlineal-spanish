2f5bf0d433c005089a7af1f48bf5725160749b7c
Dardo Sordi <dardosordi@gmail.com>
2015-02-23 13:19:45 -0300

    graficar strongs en resultados

--- php/js/search.js
+++ php/js/search.js
@@ -5,6 +5,10 @@ $(function() {
 		return;
 	}
 
+    $(".result").after('<div id="charts" style="margin-bottom: 4em; overflow: auto;"></div>');
+
+    $("#charts").append('<div id="chart1" style="width:50%;float:left;"></div>');
+
 	var books_enabled = books;
 	var selected = $('.books input[checked=checked]');
 
@@ -16,8 +20,6 @@ $(function() {
 		});
 	}
 
-	$(".result").after('<div id="chart"></div>');
-
 	var categories = [];
 	var series = [{
         name: 'Encontrados',
@@ -26,6 +28,7 @@ $(function() {
             enabled: true
         }
     }];
+
 	for (var x in books_enabled) {
 		categories.push(books[x]);
 		var count = 0;
@@ -35,12 +38,12 @@ $(function() {
 		series[0].data.push(count);
 	}
 
-    $('#chart').highcharts({
+    $('#chart1').highcharts({
         chart: {
             type: 'column'
         },
         title: {
-            text: 'Resultado'
+            text: 'Libros'
         },
         xAxis: {
             categories: categories,
@@ -68,7 +71,7 @@ $(function() {
                 }
             },
             series: {
-                pointWidth: 100
+                pointWidth: 50
             }
         },
         legend: {
@@ -86,6 +89,85 @@ $(function() {
             enabled: false
         },
         series: series
+    });
+
+
+    $("#charts").append('<div id="chart2" style="width:50%;float:left;"></div>');
+    var categories2 = [];
+	var series2 = [{
+        name: 'Strongs',
+        data: [],
+        dataLabels: {
+            enabled: true
+        }
+    }];
+
+    $('.highlight').each(function(i, e) {
+        var strongs = 'G' + $('.strongs', e).text();
+        if (categories2.indexOf(strongs) == -1) {
+            categories2.push(strongs);
+        }
+    });
+
+    categories2.sort(function(a, b) {
+        return parseInt(a.substring(1)) > parseInt(b.substring(1));
+    });
+
+    $(categories2).each(function(i, e) {
+        series2[0].data.push($('.highlight.'+e).length);
+    });
+
+    $('#chart2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Strongs'
+        },
+        xAxis: {
+            categories: categories2,
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Ocurrencias',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ''
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            },
+            series: {
+                pointWidth: 50
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -100,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: series2
     });
 
 });
